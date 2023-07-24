dfd68723bd061e05492d343489b51481230141ed
elvisowusu <67404659+elvisowusu@users.noreply.github.com>
2023-07-24 15:03:43 +0000

    added input validation functionality to the budget

--- src/components/Budget.jsx
+++ src/components/Budget.jsx
@@ -1,29 +1,54 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import { FaDollarSign } from 'react-icons/fa'
 import '../styles/Budget.css'
 
 export const Budget = () => {
-    const [percentagesArray,setPercentageArray]=useState([])
-    const [percentagevalue,setPercentageValue]=useState('')
-    const [percentageTitle,setPercentageTitle]=useState('')
+    const [percentagesArray,setPercentageArray]=useState([]);
+    const [percentageValue,setpercentageValue]=useState('');
+    const [percentageTitle,setPercentageTitle]=useState('');
+    const [percentageTracker,setPercentageTracker] = useState(0);
+    const [inputError, setInputError] =useState('');
  
 
     const percentageValueEvent=(e)=>{
-            setPercentageValue(e.target.value);
+            setpercentageValue(e.target.value);
+             // Clear the error when the user interacts with the input box
+             setInputError('');
     };
     const percentageTitleEvent=(e)=>{
             setPercentageTitle(e.target.value);
+            // Clear the error when the user interacts with the input box
+            setInputError('');
     };
 
     const addPercentageObject =()=>{
+        // Error validation
+        if (!percentageTitle || !percentageValue) {
+            setInputError('Both title and percentage fields are required.');
+            return;
+          }
+      
+          if (isNaN(percentageValue)) {
+            setInputError('Percentage must be a valid number.');
+            return;
+          }
         const newPercentageObject ={
             title:percentageTitle,
-            value:percentagevalue
+            value:percentageValue,
         };
         setPercentageArray ([...percentagesArray, newPercentageObject]);
-        setPercentageValue('');
+        setpercentageValue('');
         setPercentageTitle('');
     };
+    
+        useEffect(() =>{
+            let totalPercentages =0;
+        for(const percentagesObj of percentagesArray){
+            totalPercentages += parseFloat(percentagesObj.value)
+        }
+        setPercentageTracker(totalPercentages);
+        console.log(percentageTracker)
+        },[percentagesArray]);
     
 
 
@@ -38,12 +63,25 @@ export const Budget = () => {
             <div className='divisions'> 
                 <h3>Enter Budget percentages</h3>
                 <div>
-                 <label htmlFor="">Budget Name</label> <input onChange={percentageTitleEvent}  type="text" placeholder='title' name="" id="" /> 
-                 <label htmlFor="">Bughet percentage</label><input onChange={percentageValueEvent} type="text" placeholder='Percentage'/>
-                 
+                 <label htmlFor="">Budget Name</label> 
+                 <input 
+                   onChange={percentageTitleEvent}
+                   type="text"
+                   placeholder='title'
+                   value={percentageTitle} 
+                 /> 
+
+                 <label htmlFor="">Bughet percentage</label>
+                 <input 
+                  onChange={percentageValueEvent} 
+                  type="text"
+                  placeholder='Percentage' 
+                  value={percentageValue}
+                 />
                 </div>
+                {inputError && <div className='error-message'>{inputError}</div>}
                 <div className="addButton">
-                <button onClick={addPercentageObject} >Add</button>
+                <button onClick={addPercentageObject}>Add</button>
                 </div>
                 <div className='budget-percentages'>
                     <ul>
@@ -59,7 +97,7 @@ export const Budget = () => {
             <button className='createBudget'>Create Budget</button>
         </div>
         
-        <div className='amountsPerDivision'>
+        <div className='newBudget'>
             The total displays will appear here after the create budget button is clicked 
         </div>
         
