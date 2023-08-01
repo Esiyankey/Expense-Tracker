8b6e840ae4d5ac5eca4432b0dc911e01a7d5ca5c
elvisowusu <owusuelvisgyasi@gmail.com>
2023-08-01 13:40:23 +0000

    SECOND MAJOR UPDATE

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,4 +1,4 @@
-// import React, { useState,useEffect } from 'react';
+import React, { useState,useEffect } from 'react';
 import '../styles/Expenses.css';
 import {db} from '../config/firebase'
 import { doc,onSnapshot,collection,setDoc, Timestamp,deleteDoc} from 'firebase/firestore';
