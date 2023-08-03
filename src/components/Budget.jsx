0f16e454b6ad53f34b28fe78633b35f135689229
elvisowusu <owusuelvisgyasi@gmail.com>
2023-08-03 08:41:44 +0000

    budget commented

--- src/components/Budget.jsx
+++ src/components/Budget.jsx
@@ -1,11 +1,12 @@
-import React, { useState, useEffect,useMemo } from 'react';
+// // Import necessary modules and components
+import React, { useState, useEffect, useMemo } from 'react';
 import '../styles/Budget.css';
-import {db} from '../config/firebase'
-import { doc,onSnapshot,collection,setDoc} from 'firebase/firestore';
+import { db } from '../config/firebase';
+import { doc, onSnapshot, collection, setDoc } from 'firebase/firestore';
 
-
-
-export const Budget = ({incomeValue,setIncomeValue,totalBudget,setTotalBudget}) => {
+// Budget component
+export const Budget = ({ incomeValue, setIncomeValue, totalBudget, setTotalBudget }) => {
+  // Constants and States
   const MAX_PERCENTAGE = 100;
   const [percentagesArray, setPercentageArray] = useState([]);
   const [percentageValue, setPercentageValue] = useState('');
@@ -13,11 +14,7 @@ export const Budget = ({incomeValue,setIncomeValue,totalBudget,setTotalBudget}) 
   const [percentageTracker, setPercentageTracker] = useState(0);
   const [inputError, setInputError] = useState('');
   const [incomeInputError, setIncomeInputError] = useState('');
-  const [budgetsArray,setBudgetsArray] = useState([])
-
-
-
-  
+  const [budgetsArray, setBudgetsArray] = useState([]);
 
   // Event handler for percentage value input
   const percentageValueEvent = (e) => {
@@ -31,21 +28,9 @@ export const Budget = ({incomeValue,setIncomeValue,totalBudget,setTotalBudget}) 
     setInputError('');
   };
 
-  // Event handler for income value input
-  // const handleIncomeValueChange = (e) => {
-  //   const input = e.target.value;
-  //   const isValidInput = /^\d*\.?\d*$/.test(input);
-
-  //   if (isValidInput) {
-  //     setIncomeValue(input);
-  //     setIncomeInputError('');
-  //   } else {
-  //     setIncomeInputError('Income value must be a valid number (float or integer).');
-  //   }
-  // };
-
   // Function to add a new budget percentage object
   const addPercentageObject = () => {
+    // Validation checks for input fields
     if (!percentageTitle || !percentageValue) {
       setInputError('Both title and percentage fields are required.');
       return;
@@ -68,11 +53,10 @@ export const Budget = ({incomeValue,setIncomeValue,totalBudget,setTotalBudget}) 
       return;
     }
 
+    // Add new percentage object to the array and update the tracker
     setPercentageArray([...percentagesArray, newPercentageObject]);
     setPercentageTracker(newTotalPercentage);
-
   };
-  
 
   // Calculate the total percentage when percentagesArray changes
   useEffect(() => {
@@ -84,48 +68,32 @@ export const Budget = ({incomeValue,setIncomeValue,totalBudget,setTotalBudget}) 
     console.log(percentageTracker); // Note: This may show the previous value due to closure, but the correct value is being used.
   }, [percentagesArray]);
 
-  // if (!incomeValue) {
-  //   setIncomeInputError('Income value is required.');
-  //   return;
-  // }
-  // const percentageAsNumber = parseFloat(percentageValue);
-  // if (isNaN(percentageAsNumber) || percentageAsNumber <= 0) {
-  //   alert('Please enter a valid  percentage.');
-  //   return;
-  // }
-
-  const AddBudget=async()=>{
+  // Function to add a new budget to Firestore
+  const AddBudget = async () => {
     if (percentageTitle.trim() !== "" && percentageValue.trim() !== "") {
       try {
-        //init new doc
-
+        // Initialize new document
         const newDoc = doc(collection(db, "Budget"));
         const newBudget = {
-          id:newDoc.id,
+          id: newDoc.id,
           Title: percentageTitle,
           Value: percentageValue,
-          Income : incomeValue,
+          Income: incomeValue,
           deleted: false,
-          
         };
+        // Set the new budget document in Firestore
         await setDoc(newDoc, newBudget);
         setPercentageValue('');
         setPercentageTitle('');
         alert('Budget added successfully.');
-
       } catch (e) {
-        alert(" Error adding budget");
+        alert("Error adding budget");
         console.error("Error adding document: ", e);
       }
-      
     }
-    
-  }
+  };
 
-
-
-
-
+  // Function to calculate the total budget based on budgetsArray
   const calculateTotalBudget = () => {
     let total = 0;
     for (const Budget of budgetsArray) {
@@ -133,135 +101,128 @@ export const Budget = ({incomeValue,setIncomeValue,totalBudget,setTotalBudget}) 
     }
     return total;
   };
+
   // Memoize the total expenses calculation using useMemo
   const totalBudgetMemo = useMemo(() => calculateTotalBudget(), [budgetsArray]);
 
-
-
-
-
-
-
-  useEffect(()=>{
-    const fetchBudget=async()=>{
-      const budgetCollection= collection(db,'Budget');
-      onSnapshot(budgetCollection,(querySnapshot)=>{
-        const budgetArray =[];
-        querySnapshot.forEach((doc)=>{budgetArray.push(doc.data())});
-        
+  // Fetch budget data from Firestore on component mount
+  useEffect(() => {
+    const fetchBudget = async () => {
+      const budgetCollection = collection(db, 'Budget');
+      onSnapshot(budgetCollection, (querySnapshot) => {
+        const budgetArray = [];
+        querySnapshot.forEach((doc) => { budgetArray.push(doc.data()) });
         setBudgetsArray(budgetArray);
-   
       });
     };
     fetchBudget();
-  },[])
+  }, []);
 
+  // Update totalBudget when totalBudgetMemo changes
   useEffect(() => {
     setTotalBudget(totalBudgetMemo);
   }, [totalBudgetMemo, setTotalBudget]);
 
-
-
+  // JSX to render the component
   return (
-      <div className='Main'>
-        {/* Display total income */}
-        <div className='balance'>
-          <h3>Total Income:</h3>
-          {/* Use toFixed(2) to display totalIncome with 2 decimal places */}
-          {incomeValue}
-        </div>
-    
-        <div className='bottom'>
-          <div className='budget-divisions'>
-            {/* Input field for Total Income */}
-            <div className="incomeValue">
-              <label id='Income'>Enter Total Income</label>
+    <div className='Main'>
+      {/* Display total income */}
+      <div className='balance'>
+        <h3>Total Income:</h3>
+        {/* Use toFixed(2) to display totalIncome with 2 decimal places */}
+        {incomeValue}
+      </div>
+
+      <div className='bottom'>
+        <div className='budget-divisions'>
+          {/* Input field for Total Income */}
+          <div className="incomeValue">
+            <label id='Income'>Enter Total Income</label>
+            <input
+              className='income'
+              type="text"
+              onChange={(e) => {
+                setIncomeValue(e.target.value)
+              }}
+              value={incomeValue}
+            />
+          </div>
+
+          {/* Display error message for income input */}
+          {incomeInputError && <div className='error-message'>{incomeInputError}</div>}
+
+          {/* Input field for Budget percentages */}
+          <div className='divisions'>
+            <h3>Enter Budget percentages</h3>
+            <div className='cylinder'>
+              <div
+                className='fill'
+                style={{ width: `${(percentageTracker / MAX_PERCENTAGE) * 100}%` }}
+              >
+                {percentageTracker}%
+              </div>
+            </div>
+            <div className='inputs'>
+              {/* Input field for Budget Name */}
+              <label>Budget Name</label>
               <input
-                className='income'
+                onChange={percentageTitleEvent}
+                onFocus={() => setInputError('')}
                 type="text"
-                // onChange={handleIncomeValueChange}
-                onChange={(e)=>{
-                  setIncomeValue(e.target.value)
-                }}
-                value={incomeValue}
+                placeholder='title'
+                value={percentageTitle}
+              />
+
+              {/* Input field for Budget Percentage */}
+              <label htmlFor="">Budget percentage</label>
+              <input
+                onChange={percentageValueEvent}
+                onFocus={() => setInputError('')}
+                type="text"
+                placeholder='Percentage'
+                value={percentageValue}
               />
             </div>
-    
-            {/* Display error message for income input */}
-            {incomeInputError && <div className='error-message'>{incomeInputError}</div>}
-    
-            {/* Input field for Budget percentages */}
-            <div className='divisions'>
-              <h3>Enter Budget percentages</h3>
-              <div className='cylinder'>
-                <div
-                  className='fill'
-                  style={{ width: `${(percentageTracker / MAX_PERCENTAGE) * 100}%` }}
-                >
-                  {percentageTracker}%
-                </div>
-              </div>
-              <div className='inputs'>
-                {/* Input field for Budget Name */}
-                <label >Budget Name</label>
-                <input
-                  onChange={percentageTitleEvent}
-                  onFocus={() => setInputError('')}
-                  type="text"
-                  placeholder='title'
-                  value={percentageTitle}
-                />
-    
-                {/* Input field for Budget Percentage */}
-                <label htmlFor="">Budget percentage</label>
-                <input
-                  onChange={percentageValueEvent}
-                  onFocus={() => setInputError('')}
-                  type="text"
-                  placeholder='Percentage'
-                  value={percentageValue}
-                />
-              </div>
-    
-              {/* Display error message for percentage input */}
-              {inputError && <div className='error-message'>{inputError}</div>}
-    
-              {/* Add button to add a new budget percentage */}
-              <div className="addButton">
-                <button onClick={addPercentageObject}>Add</button>
-              </div>
-    
-              {/* Display the list of budget percentages */}
-              <div className='budget-percentages'>
-                <ul>
-                  {percentagesArray.map((percentagesObj, index) => (
-                    <li key={index} className='title-percentages'>
-                      <h3>{percentagesObj.title}</h3>
-                      <p>{percentagesObj.value}</p>
-                    </li>
-                  ))}
-                </ul>
-              </div>
+
+            {/* Display error message for percentage input */}
+            {inputError && <div className='error-message'>{inputError}</div>}
+
+            {/* Add button to add a new budget percentage */}
+            <div className="addButton">
+              <button onClick={addPercentageObject}>Add</button>
             </div>
-    
-            {/* Create Budget button */}
-            <div className="createBudget">
-              <button className='createButton' onClick={AddBudget}>Create Budget</button>
+
+            {/* Display the list of budget percentages */}
+            <div className='budget-percentages'>
+              <ul>
+                {percentagesArray.map((percentagesObj, index) => (
+                  <li key={index} className='title-percentages'>
+                    <h3>{percentagesObj.title}</h3>
+                    <p>{percentagesObj.value}</p>
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
-    
-          {/* Placeholder for displaying the created budget */}
-          <div className='newBudget'>
-          <ul>
-              {budgetsArray.map((items, index) => (
-                    <li key={index} className='title-percentages'>
-                      <h4>{items.Title}</h4>
-                      <p>{(items.Value/100)*parseFloat(items.Income)}</p>
-                    </li>
-                  ))}
-            </ul>
+
+          {/* Create Budget button */}
+          <div className="createBudget">
+            <button className='createButton' onClick={AddBudget}>Create Budget</button>
           </div>
         </div>
+
+        {/* Placeholder for displaying the created budget */}
+        <div className='newBudget'>
+          <ul>
+            {budgetsArray.map((items, index) => (
+              <li key={index} className='title-percentages'>
+                <h4>{items.Title}</h4>
+                <p>{(items.Value / 100) * parseFloat(items.Income)}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
+    </div>
   );
 };
